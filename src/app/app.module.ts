819737fc03da5ae794e681581531a69236054cb4
Prince <vasoyaprince177@gmail.com>
2025-07-17 18:12:15 +0530

    add allmodules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,47 @@ import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 import { ServiceComponent } from './pages/service/service.component';
 import { SearchComponent } from './pages/search/search.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { CardModule } from 'primeng/card';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
+import { PasswordModule } from 'primeng/password';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { DropdownModule } from 'primeng/dropdown';
+import { MenubarModule } from 'primeng/menubar';
+import { SplitterModule } from 'primeng/splitter';
+import { SkeletonModule } from 'primeng/skeleton';
+import { TagModule } from 'primeng/tag';
+import { DataViewModule } from 'primeng/dataview';
+import { CalendarModule } from 'primeng/calendar';
+import { DialogModule } from 'primeng/dialog';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { AvatarGroupModule } from 'primeng/avatargroup';
+import { ToastModule } from 'primeng/toast';
+import { MenuModule } from 'primeng/menu';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { AvatarModule } from 'primeng/avatar';
+import { DynamicDialogModule } from 'primeng/dynamicdialog';
+import { DialogService } from 'primeng/dynamicdialog';
+import { TabViewModule } from 'primeng/tabview';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { SidebarModule } from 'primeng/sidebar';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { DividerModule } from 'primeng/divider';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { FileUploadModule } from 'primeng/fileupload';
+import { TableModule } from 'primeng/table';
+import { CheckboxModule } from 'primeng/checkbox';
+import { ChipModule } from 'primeng/chip';
+import { AccordionModule } from 'primeng/accordion';
+import { SliderModule } from 'primeng/slider';
+import { CarouselModule } from 'primeng/carousel';
+import { ScrollTopModule } from 'primeng/scrolltop';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -50,9 +91,57 @@ import { SearchComponent } from './pages/search/search.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    InputTextModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ButtonModule,
+    CardModule,
+    NgxMaskDirective,
+    PasswordModule,
+    FloatLabelModule,
+    DropdownModule,
+    MenubarModule,
+    SplitterModule,
+    SkeletonModule,
+    TagModule,
+    DataViewModule,
+    InputTextModule,
+    DropdownModule,
+    FloatLabelModule,
+    CalendarModule,
+    DialogModule,
+    ConfirmDialogModule,
+    ToastModule,
+    MenuModule,
+    InputTextareaModule,
+    NgxMaskDirective,
+    AvatarModule,
+    AvatarGroupModule,
+    DynamicDialogModule,
+    TabViewModule,
+    ProgressBarModule,
+    SidebarModule,
+    ProgressSpinnerModule,
+    DividerModule,
+    SelectButtonModule,
+    FileUploadModule,
+    TableModule,
+    CheckboxModule,
+    ChipModule,
+    AccordionModule,
+    SliderModule,
+    CarouselModule,
+    ScrollTopModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    ConfirmationService,
+    MessageService,
+    DialogService,
+    provideNgxMask(),
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
